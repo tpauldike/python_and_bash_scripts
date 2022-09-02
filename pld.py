0c714498e9a820a480f227acb5064b1a8e665c0d
tpauldike <topman4loveworld@gmail.com>
2022-09-02 10:06:46 +0000

    A program that prints some selected portions from a string

---
+++ pld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+sentence = ('This is a PLD session with cohort 7, 8 and 9, '
+            'it was supposed to be for cohort seven but others joined')
+
+#print(sentence)
+print(sentence[27:33], sentence[43], sentence[10:13], sentence[22:26], sentence[89:95])
+
+'''
+The program prints 'cohort 9 PLD with others' from the string stored
+in the variable, sentence
+'''
