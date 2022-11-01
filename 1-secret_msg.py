ee593c3485c54307de901047586b620d326062fc
tpauldike <topman4loveworld@gmail.com>
2022-11-01 08:03:46 -0700

    files made portable

--- 1-secret_msg.py
+++ 1-secret_msg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
   A program that converts messages written in text to
 ASCII codes, thereby making it a secret message.
--- 2-decode_ascii.py
+++ 2-decode_ascii.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
    A program that decodes messages written in ASCII codes and changes them
 into readable characters
