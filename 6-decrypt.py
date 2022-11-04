1f2cc7ab0933e5857c79f18503b022e0f01c97d9
tpauldike <topman4loveworld@gmail.com>
2022-11-04 05:31:54 -0700

    decrypt the encrypted message by -7 shifts

---
+++ 6-decrypt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# A program that decrypts what was encrypted by '5-encrypt.py'
+encrypted = input("Paste the encrypted message that you wish to decrypt:\n:$ ")
+shift = 7
+decrypted = ""
+
+for words in encrypted:
+    if words.isalpha():
+        code = ord(words)
+        code -= shift
+        if words.isupper():
+            if code > ord('Z'):
+                code -= 26
+            if code < ord('A'):
+                code += 26
+        else:
+            if code > ord('z'):
+                code -= 26
+            if code < ord('a'):
+                code += 26
+        decrypted += chr(code)
+    else:
+        decrypted += words
+print("Message Decrypted!\n---\n", decrypted, end="\n---\n")
